310b02e657a07f168f08f89e8ebf15334b08e64a
Jared Rodriguez <jared.rodriguez@rackspace.com>
2017-10-20 00:32:41 -0500

    Fixed tests

--- tests/unit/raid_abstraction/test_storcli.py
+++ tests/unit/raid_abstraction/test_storcli.py
@@ -8,59 +8,61 @@ from ..base import MercuryAgentUnitTest
 
 
 class StorcliTest(MercuryAgentUnitTest):
-    @mock.patch('mercury.common.helpers.cli.run')
-    def test_run(self, mock_run):
-        mock_run.return_value = CLIResult('', '', 0)
-
+    @mock.patch('mercury.hardware.raid.interfaces.megaraid.storcli.cli')
+    def test_run(self, mock_cli):
+        mock_cli.run.return_value = CLIResult('', '', 0)
+        mock_cli.find_in_path.return_value = '/sbin/storcli64'
         s = storcli.Storcli()
         assert s.run('') == ''
 
-        mock_run.return_value = CLIResult('Error', '', 1)
+        mock_cli.run.return_value = CLIResult('Error', '', 1)
 
         self.assertRaises(storcli.StorcliException, s.run, *('', ))
 
-    @mock.patch('mercury.common.helpers.cli.run')
-    def test_run_json(self, mock_run):
+    @mock.patch('mercury.hardware.raid.interfaces.megaraid.storcli.cli')
+    def test_run_json(self, mock_cli):
         with open(os.path.join(os.path.dirname(__file__),
                                '../resources/storcli.json')) as fp:
             json_data = fp.read()
 
-        mock_run.return_value = CLIResult(json_data, '', 0)
-
+        mock_cli.run.return_value = CLIResult(json_data, '', 0)
+        mock_cli.find_in_path.return_value = '/sbin/storcli64'
         s = storcli.Storcli()
         assert isinstance(s.run_json('/call show all'), dict)
 
-        mock_run.return_value = CLIResult('not_valid_json', '', 0)
+        mock_cli.run.return_value = CLIResult('not_valid_json', '', 0)
 
         self.assertRaises(storcli.StorcliException, s.run_json, *('', ))
 
-    @mock.patch('mercury.common.helpers.cli.run')
-    def test_controllers(self, mock_run):
+    @mock.patch('mercury.hardware.raid.interfaces.megaraid.storcli.cli')
+    def test_controllers(self, mock_cli):
         with open(os.path.join(os.path.dirname(__file__),
                                '../resources/storcli.json')) as fp:
             json_data = fp.read()
 
-        mock_run.return_value = CLIResult(json_data, '', 0)
-
+        mock_cli.run.return_value = CLIResult(json_data, '', 0)
+        mock_cli.find_in_path.return_value = '/sbin/storcli64'
         s = storcli.Storcli()
         assert isinstance(s.controllers, list)
 
-        mock_run.return_value = CLIResult('{}', '', 0)
+        mock_cli.run.return_value = CLIResult('{}', '', 0)
 
         def _wrapper():
             return s.controllers
 
         self.assertRaises(storcli.StorcliException, _wrapper)
 
-    @mock.patch('mercury.common.helpers.cli.run')
-    def test_delete(self, mock_run):
-        mock_run.return_value = CLIResult('', '', 0)
+    @mock.patch('mercury.hardware.raid.interfaces.megaraid.storcli.cli')
+    def test_delete(self, mock_cli):
+        mock_cli.run.return_value = CLIResult('', '', 0)
+        mock_cli.find_in_path.return_value = '/sbin/storcli64'
         s = storcli.Storcli()
         assert s.delete(0) == ''
 
-    @mock.patch('mercury.common.helpers.cli.run')
-    def test_add(self, mock_run):
-        mock_run.return_value = CLIResult('', '', 0)
+    @mock.patch('mercury.hardware.raid.interfaces.megaraid.storcli.cli')
+    def test_add(self, mock_cli):
+        mock_cli.run.return_value = CLIResult('', '', 0)
+        mock_cli.find_in_path.return_value = '/sbin/storcli64'
 
         s = storcli.Storcli()
         assert s.add(0, 'r0', '32:0-1') == ''
@@ -71,6 +73,6 @@ class StorcliTest(MercuryAgentUnitTest):
         with open(os.path.join(os.path.dirname(__file__), '../resources/storcli_err.txt')) as fp:
             error_data = fp.read()
 
-        mock_run.return_value = CLIResult(error_data, '', 0)
+        mock_cli.run.return_value = CLIResult(error_data, '', 0)
 
         self.assertRaises(storcli.StorcliException, s.add, *(0, '', ''))
